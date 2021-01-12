fdd6b596647a6d86789b7a3e3998a075de110948
Damian Woźniak <127375@student.uni.opole.pl>
2021-01-12 13:34:41 +0100

    Dodaj pliki projektów.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lista7_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int k = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            BlinkingLamps(lamp1);
+        }
+        public void BlinkingLamps(Image image)
+        {
+            DoubleAnimation opacityAnimation = new DoubleAnimation
+            {
+                From = 1.0,
+                To = 0.3,
+                Duration = new Duration(TimeSpan.FromSeconds(1)),
+                AutoReverse = true,
+                RepeatBehavior = new RepeatBehavior(100),
+                BeginTime = new TimeSpan(0, 0, 0, 0, k)
+            };
+            Storyboard storyboard = new Storyboard();
+            storyboard.Children.Add(opacityAnimation);
+            Storyboard.SetTarget(opacityAnimation, image);
+            Storyboard.SetTargetProperty(opacityAnimation, new PropertyPath("Opacity"));
+            storyboard.Begin(image);
+        }
+    }
+}
